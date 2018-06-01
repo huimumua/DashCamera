2d51d3f928e81fdb79f88fa038fba4565cc72c58
Mark Xiang (向新君_Askey_CT) <mark_xiang@askey.com>
2018-06-01 11:17:59 +0800

    get InsuranceTerm by callback

--- app/src/main/java/com/askey/dvr/cdr7010/dashcam/jvcmodule/local/LocalJvcStatusManager.java
+++ app/src/main/java/com/askey/dvr/cdr7010/dashcam/jvcmodule/local/LocalJvcStatusManager.java
@@ -6,7 +6,10 @@ import com.askey.dvr.cdr7010.dashcam.application.DashCamApplication;
 import com.askey.dvr.cdr7010.dashcam.jvcmodule.local.JvcStatusParams.JvcStatus;
 import com.askey.dvr.cdr7010.dashcam.jvcmodule.local.JvcStatusParams.JvcStatusParam;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.EnumMap;
+import java.util.List;
 
 /***
  * Company: Chengdu Skysoft Info&Tech Co., Ltd.
@@ -18,17 +21,32 @@ import java.util.EnumMap;
  * @see
  ***/
 public class LocalJvcStatusManager {
+    private static List<LocalJvcStatusCallback> mCallbacks = Collections.synchronizedList(new ArrayList<LocalJvcStatusCallback>());
 
-    public static EnumMap<JvcStatusParam, Object> getInsuranceTerm(){
+    public static EnumMap<JvcStatusParam, Object> getInsuranceTerm(LocalJvcStatusCallback callback){
         Context appContext = DashCamApplication.getAppContext();
         EnumMap<JvcStatusParam, Object> object = (EnumMap<JvcStatusParam, Object>) ObjectPreference.getObjectFromShare(appContext, JvcStatus.INSURANCE_TERM.getName());
+        if(object != null && callback != null){
+            mCallbacks.add(callback);
+            callback.onDataArriving(object);
+        }
+
         return object;
     }
 
     public static void setInsuranceTerm(EnumMap<JvcStatusParam, Object> enumMap){
         Context appContext = DashCamApplication.getAppContext();
         ObjectPreference.setObjectToShare(appContext, enumMap, JvcStatus.INSURANCE_TERM.getName());
+
+        for(LocalJvcStatusCallback callback : mCallbacks){
+            callback.onDataArriving(enumMap);
+        }
     }
 
+
+
+    public interface LocalJvcStatusCallback{
+        void onDataArriving(EnumMap<JvcStatusParam, Object> enumMap);
+    }
 
 }
