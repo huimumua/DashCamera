ec5a0bbd47319523641bb45ba7a9c0675907c3ac
Mark Xiang (向新君_Askey_CT) <mark_xiang@askey.com>
2018-06-04 18:10:17 +0800

    eventsending to broadcast

---
+++ app/src/main/java/com/askey/dvr/cdr7010/dashcam/jvcmodule/jvckenwood/JvcEventSending.java
@@ -0,0 +1,70 @@
+package com.askey.dvr.cdr7010.dashcam.jvcmodule.jvckenwood;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.askey.dvr.cdr7010.dashcam.application.DashCamApplication;
+import com.askey.dvr.cdr7010.dashcam.util.Logg;
+
+import java.util.ArrayList;
+
+/***
+ * Company: Chengdu Skysoft Info&Tech Co., Ltd.
+ * Copyright ©2014-2018 Chengdu Skysoft Info&Tech Co., Ltd.
+ * Created by Mark on 2018/5/16.
+
+ * @since:JDK1.6
+ * @version:1.0
+ * @see
+ ***/
+public class JvcEventSending {
+    public static final String ACTION_EVENT_NOTIFY_ADAS = "com.jvckenwood.eventsending.EVENT_NOTIFY_ADAS";
+    public static final String ACTION_EVENT_NOTIFY_EVENT_DETECT = "com.jvckenwood.eventsending.EVENT_NOTIFY_EVENT_DETECT";
+    public static final String ACTION_EVENT_RECORD_RESPONSE = "com.jvckenwood.eventsending.EVENT_RECORD_RESPONSE";
+
+    private static final String LOG_TAG = "JvcEventSending";
+    private static JvcEventSending JvcEventSending;
+    private final Context mAppContext;
+
+    private JvcEventSending() {
+        mAppContext = DashCamApplication.getAppContext();
+    }
+
+    public static JvcEventSending getInstance(){
+        if(JvcEventSending == null)
+            JvcEventSending = new JvcEventSending();
+
+        return JvcEventSending;
+    }
+
+    public void notifyAdas(int eventType, long timeStamp){
+        Logg.d(LOG_TAG, "notifyAdas: eventType=" + eventType + ", timeStamp=" + timeStamp);
+        Intent intent = new Intent(ACTION_EVENT_NOTIFY_ADAS);
+        intent.putExtra("eventType", eventType);
+        intent.putExtra("timeStamp", timeStamp);
+        sendOutBroadcast(intent);
+    }
+
+    public void notivyEventDetect(int eventType, long timeStamp){
+        Logg.d(LOG_TAG, "notivyEventDetect: eventType=" + eventType + ", timeStamp=" + timeStamp);
+        Intent intent = new Intent(ACTION_EVENT_NOTIFY_EVENT_DETECT);
+        intent.putExtra("eventType", eventType);
+        intent.putExtra("timeStamp", timeStamp);
+        sendOutBroadcast(intent);
+    }
+
+    public void recordResponse(int eventNo, ArrayList<Integer> result, ArrayList<String> recordPath){
+        Logg.d(LOG_TAG, "recordResponse: eventNo=" + eventNo);
+        Intent intent = new Intent(ACTION_EVENT_RECORD_RESPONSE);
+        intent.putExtra("eventNo", eventNo);
+        intent.putIntegerArrayListExtra("result", result);
+        intent.putStringArrayListExtra("recordPath", recordPath);
+        sendOutBroadcast(intent);
+    }
+
+    private void sendOutBroadcast(Intent intent){
+        mAppContext.sendBroadcast(intent);
+    }
+
+
+}
