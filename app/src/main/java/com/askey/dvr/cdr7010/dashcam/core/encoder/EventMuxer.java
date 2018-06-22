f8564f32cc71520333e076ce09dfedb9d8abb98c
Liuyi Luo (羅流毅_Askey_CT) <liuyi_luo@askey.com>
2018-06-22 17:55:25 +0800

    移除多余的log

--- app/src/main/java/com/askey/dvr/cdr7010/dashcam/core/encoder/EventMuxer.java
+++ app/src/main/java/com/askey/dvr/cdr7010/dashcam/core/encoder/EventMuxer.java
@@ -234,9 +234,6 @@ public class EventMuxer implements Runnable{
         try {
             fin = new RandomAccessFile(file, "r");
             int index = fin.readInt();
-            if (!isFirst) {
-                Logg.d(LOG_TAG, "index=" + index);
-            }
             if (!isFirst && (index - slice_index != 1)) {
                 Logg.e(LOG_TAG, "slice index error: prev=" + slice_index + "  current=" + index);
             }
