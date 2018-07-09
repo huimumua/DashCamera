be4f0e7c18944e71d1284e92c00d718d3cad68a6
Antony Zhu (朱俊林_Askey_CT) <antony_zhu@askey.com>
2018-07-09 18:19:54 +0800

    bugfix(PUCDR-1378):fix [initialization] After doing "設定初期化" setup, DUT popup "SDカードが挿入されていません" error indication but SD card already contained in DUT.

--- app/src/main/java/com/askey/dvr/cdr7010/dashcam/util/SDcardHelper.java
+++ app/src/main/java/com/askey/dvr/cdr7010/dashcam/util/SDcardHelper.java
@@ -6,11 +6,14 @@ import com.askey.dvr.cdr7010.dashcam.logic.GlobalLogic;
 import com.askey.dvr.cdr7010.dashcam.service.EventManager;
 import com.askey.dvr.cdr7010.dashcam.ui.utils.UIElementStatusEnum;
 
+import static com.askey.dvr.cdr7010.dashcam.ui.utils.UIElementStatusEnum.SDcardStatusType.SDCARD_MOUNTED;
 import static com.askey.dvr.cdr7010.dashcam.ui.utils.UIElementStatusEnum.SDcardStatusType.SDCARD_REMOVED;
 
 public class SDcardHelper{
+    private static final String TAG = SDcardHelper.class.getSimpleName();
     public static boolean isSDCardAvailable(){
         String status = Environment.getExternalStorageState();
+        Logg.d(TAG,"sdcard status="+status);
         if (status.equalsIgnoreCase(Environment.MEDIA_REMOVED)
                 || status.equalsIgnoreCase(Environment.MEDIA_BAD_REMOVAL)
                 || status.equalsIgnoreCase(Environment.MEDIA_UNMOUNTED)) {
@@ -18,10 +21,18 @@ public class SDcardHelper{
         }
         return true;
     }
+    public static boolean isSDCardEnable()
+    {
+        return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
+
+    }
     public static void checkSdCardExist(){
         if (!isSDCardAvailable()) {
             GlobalLogic.getInstance().setSDCardCurrentStatus(SDCARD_REMOVED);
             EventManager.getInstance().handOutEventInfo(110);
+        }
+        if(isSDCardEnable()){
+            GlobalLogic.getInstance().setSDCardCurrentStatus(SDCARD_MOUNTED);
         }
     }
     public static void handleSdcardAbnormalEvent(){
