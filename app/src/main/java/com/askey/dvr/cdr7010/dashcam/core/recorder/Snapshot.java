1b87742638a5be939813979370940247b6e08303
Navas Li (李纳_Askey_CT) <navas.lii@askey.com>
2018-09-06 11:09:07 +0800

    fix bug:get nmea info error.

--- app/src/main/java/com/askey/dvr/cdr7010/dashcam/core/recorder/Snapshot.java
+++ app/src/main/java/com/askey/dvr/cdr7010/dashcam/core/recorder/Snapshot.java
@@ -35,7 +35,7 @@ public class Snapshot {
         if (!new File(videoFilePath).exists()) {
             Logg.e(TAG, "video file not exist.");
             if (listener != null) {
-                listener.onPictureTake(new ArrayList<String>(0));
+                listener.onPictureTake(new ArrayList<>(0));
             }
             return;
         }
@@ -49,7 +49,6 @@ public class Snapshot {
             }
             return;
         }
-
         long pts = timeStamp - 3000;
         long pos = firstTime;
         final List<String> fileNames = new ArrayList<>(3);
@@ -62,50 +61,56 @@ public class Snapshot {
                 pos += SEEK_STEP;
                 continue;
             }
-
             Bitmap bmp = retriever.getFrameAtTime(pos);
-            if (bmp != null) {
-                FileOutputStream bos = null;
-                try {
-                    bos = new FileOutputStream(image);
-                    bmp.compress(Bitmap.CompressFormat.JPEG, 100, bos);
-                    bos.flush();
-                    bmp.recycle();
-                    ExifHelper.build(image, pts);
-                    fileNames.add(image);
-                } catch (IOException e) {
-
-                } finally {
-                    if (bos != null) {
-                        try {
-                            bos.close();
-                        } catch (IOException e) {
-                        }
-                    }
-                }
-            }
-
+            saveBitmap(pts, fileNames, image, bmp);
             pts += 3000;
             pos += SEEK_STEP;
         }
         retriever.release();
-
         String nmeaPath = videoFilePath.replace("EVENT", "SYSTEM/NMEA/EVENT").replace("mp4", "nmea");
+        saveNmea(fileNames, nmeaPath);
+        if (listener != null) {
+            listener.onPictureTake(fileNames);
+        }
+    }
+
+    private static void saveBitmap(long pts, List<String> fileNames, String image, Bitmap bmp) {
+        if (bmp != null) {
+            FileOutputStream bos = null;
+            try {
+                bos = new FileOutputStream(image);
+                bmp.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+                bos.flush();
+                bmp.recycle();
+                ExifHelper.build(image, pts);
+                fileNames.add(image);
+            } catch (IOException e) {
+                //ignore
+            } finally {
+                if (bos != null) {
+                    try {
+                        bos.close();
+                    } catch (IOException e) {
+                        //ignore
+                    }
+                }
+            }
+        }
+    }
+
+    private static void saveNmea(List<String> fileNames, String nmeaPath) {
         try {
             NMEAUtils nmeaUtils = new NMEAUtils(nmeaPath);
             nmeaUtils.setOnFinishListener(list -> {
-                for (String file:fileNames) {
+                for (String file : fileNames) {
                     String timeNeed = file.substring(file.lastIndexOf("/") + 1, file.indexOf("."));
                     Position location = NMEAUtils.getLocationFromSentences(list, timeNeed);
                     ExifHelper.build(file, location);
                 }
             });
+            nmeaUtils.start();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        }
-
-        if (listener != null) {
-            listener.onPictureTake(fileNames);
         }
     }
 }
