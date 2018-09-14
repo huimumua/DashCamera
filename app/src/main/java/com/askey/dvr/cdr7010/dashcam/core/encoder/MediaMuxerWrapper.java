bf6e269afd62926f37d2793feabe8adb37e5a205
jay.k <jay.k@cdskysoft.com>
2018-09-14 13:43:07 +0800

    修复进入菜单，拔出sd卡，退出菜单，程序崩溃的问题。

--- app/src/main/java/com/askey/dvr/cdr7010/dashcam/core/encoder/MediaMuxerWrapper.java
+++ app/src/main/java/com/askey/dvr/cdr7010/dashcam/core/encoder/MediaMuxerWrapper.java
@@ -38,7 +38,7 @@ public class MediaMuxerWrapper {
     private final String LOG_TAG;
 
     private Context mContext;
-    private final HandlerThread mHandlerThread;
+    private static HandlerThread mHandlerThread = null;
     private final Handler mHandler;
     private MediaFormat mVideoFormat;
     private MediaFormat mAudioFormat;
@@ -123,8 +123,11 @@ public class MediaMuxerWrapper {
         mEncoderCount = mStatredCount = 0;
         mIsStarted = false;
 
-        mHandlerThread = new HandlerThread("MuxerWorker-" + mConfig.cameraId());
-        mHandlerThread.start();
+        if(mHandlerThread == null){
+            mHandlerThread = new HandlerThread("MuxerWorker-" + mConfig.cameraId());
+            mHandlerThread.start();
+        }
+
         mHandler = new Handler(mHandlerThread.getLooper());
 
         mMuxerThread = new HandlerThread("MuxerThread-" + mConfig.cameraId());
@@ -297,7 +300,6 @@ public class MediaMuxerWrapper {
                 }
             }
         }
-        mHandlerThread.quitSafely();
     }
 
     /*package*/
